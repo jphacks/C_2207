f2383885b633b125c2ddf7350f96121db51eec87
Mio Nakamura <m.nakamura.264@stn.nitech.ac.jp>
2022-10-15 16:16:45 +0900

    add twitter

--- testpro/app/src/main/java/com/example/testpro/MainActivity.java
+++ testpro/app/src/main/java/com/example/testpro/MainActivity.java
@@ -8,6 +8,15 @@ package com.example.testpro;
         import android.widget.CalendarView;
         import android.widget.Toast;
 
+        import java.awt.Desktop;
+        import java.io.BufferedWriter;
+        import java.io.File;
+        import java.io.FileWriter;
+        import java.io.IOException;
+        import java.net.URISyntaxException;
+        import java.net.URL;
+        import java.util.Date;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,6 +36,37 @@ public class MainActivity extends AppCompatActivity {
                 }
         );
 
+    }
+
+
+
+
+
+        public static void openTwitter(String[] args) {
+            if(args.length<1){
+                System.out.println("検索ワードをコマンドライン引数に指定してください");
+                return;
+            }
+            try{
+                Desktop desktop=Desktop.getDesktop();
+                URL input=new URL("https://twitter.com/search?q=from%3A"+String.join("+",args[0]) + "%20since%3A" +String.join("+",args[1]) + "%20" +String.join("+",args[2]) +"&src=typed_query&f=top");
+                BufferedWriter bw=new BufferedWriter(new FileWriter(new File("./search.log"), true));
+                bw.write("["+new Date()+"] "+input.toString());
+                bw.newLine();
+                bw.close();
+                desktop.browse(input.toURI());
+            }catch(UnsupportedOperationException e){
+                System.out.println("この環境ではサポートされていません。");
+                e.printStackTrace();
+
+            }catch(URISyntaxException e){
+                System.out.println("検索ワードが不正です。");
+                e.printStackTrace();
+            }catch(IOException e){
+                System.out.println("エラーが発生しました");
+                e.printStackTrace();
+            }
+        }
     }
 
 }
