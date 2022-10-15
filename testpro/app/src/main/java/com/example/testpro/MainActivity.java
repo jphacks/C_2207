37c42ffbf7826034f20fe57240d0097d13f12587
suzukikaho <mugimameawa2828@gmail,com>
2022-10-15 15:32:42 +0900

    testCalender_suzuki1

--- testpro/app/src/main/java/com/example/testpro/MainActivity.java
+++ testpro/app/src/main/java/com/example/testpro/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.testpro;
 
-import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+        import androidx.annotation.NonNull;
+        import androidx.appcompat.app.AppCompatActivity;
+
+        import android.os.Bundle;
+        import android.widget.CalendarView;
+        import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CalendarView calendar = findViewById(R.id.calendar);
+        calendar.setOnDateChangeListener(
+                new CalendarView.OnDateChangeListener() {
+                    @Override
+                    public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int date) {
+                        String message = year + "/" + (month + 1) + "/" + date;
+                        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+
     }
+
 }
