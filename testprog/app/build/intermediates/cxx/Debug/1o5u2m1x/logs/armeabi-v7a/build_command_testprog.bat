1f87926353659db940df66bf919d1ca32f956109
momongasleep <momonga_s3@outlook.jp>
2022-10-19 10:52:32 +0900

    twitter4jの導入

---
+++ testprog/app/build/intermediates/cxx/Debug/1o5u2m1x/logs/armeabi-v7a/build_command_testprog.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\momon\\AppData\\Local\\Android\\Sdk\\cmake\\3.18.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\momon\\AndroidStudioProjects\\C_2207\\testprog\\app\\.cxx\\Debug\\1o5u2m1x\\armeabi-v7a" ^
+  testprog
