bf339c54ee2b1df79337946f0d987d86b57457f0
suzukikaho <mugimameawa2828@gmail,com>
2022-10-20 19:45:33 +0900

    add setting2

---
+++ testprog/app/build/intermediates/cxx/Debug/733g3w3s/logs/arm64-v8a/build_command_testprog.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\suzukikaho\\AppData\\Local\\Android\\Sdk\\cmake\\3.18.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\suzukikaho\\AndroidStudioProjects\\C_2207\\testprog\\app\\.cxx\\Debug\\733g3w3s\\arm64-v8a" ^
+  testprog
